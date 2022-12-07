21751df52c228df62edf4dd6a2ea7498e6952558
GastonZ <gastonzappullaryu@gmail.com>
2022-12-07 11:19:35 -0300

    working

--- navigation/screens/Cities.js
+++ navigation/screens/Cities.js
@@ -2,6 +2,9 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 export default function Cities({navigation}) {
+
+  
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text style={{ fontSize: 32, fontWeight: 'bold'}}>Cities</Text>
